0aeb7ca69368535d0a6b9d326f67e5ce6b2474d4
makmakmakxd <akif.kilic@ug.bilkent.edu.tr>
2020-12-06 17:47:34 +0300

    restaurant is finished

--- src/routes/restaurant.js
+++ src/routes/restaurant.js
@@ -5,23 +5,27 @@ import express from 'express';
 // import { v4 as uuid } from 'uuid';
 
 // import validator from 'validator';
+import { v4 as uuid } from 'uuid';
 import { /* generateJwtToken */ reduceUserDetails } from '../lib/utils';
 import constraints from '../lib/constraints';
 import { db } from '../lib/clients';
+
+const moment = require('moment');
+
 // import restaurant from '../topics/restaurant';
 
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', async (req, res, /* next */) => {
-  const { uuid } = req.query;
+  const { resUuid } = req.query;
 
-  if (!uuid) {
+  if (!resUuid) {
     const err = constraints.errors.MISSING_ARGS;
     return res.status(err.code).send(err);
   }
 
-  const restaurantDetails = await db.Restaurant.findByPk(uuid, { include: { as: 'Menu', model: db.Item } });
+  const restaurantDetails = await db.Restaurant.findByPk(resUuid, { include: { as: 'Menu', model: db.Item } });
   const reduced = reduceUserDetails(restaurantDetails.dataValues);
   return res.send(reduced);
 });
@@ -33,6 +37,24 @@ router.get('/me', async (req, res, /* next */) => {
   const reduced = reduceUserDetails(restaurantDetails.dataValues);
   return res.send(reduced);
 });
+
+
+router.post('/me', async (req, res, /* next */) => {
+  const { restaurant } = req;
+  const payload = req.body;
+  await db.Restaurant.update( 
+    {
+      name: payload.name, 
+      address : payload.price,
+      phoneNumber : payload.phoneNumber , 
+      email : payload.email 
+    }, {
+      where: { uuid : restaurant.uuid}
+    });
+ 
+  return res.send({ success: true });
+});
+ 
 
 router.get('/menu', async (req, res,) => {
   const { restaurant } = req;
@@ -53,5 +75,72 @@ router.get('/orders', async (req, res, /* next */) => {
 
   return res.send({ orders });
 });
+
+
+router.get('/item', async (req, res, /* next */) => {
+
+  const { restaurant } = req;
+  const {itemUuid} = req.query;
+  
+  const item = await db.Item.findAll({
+    where: {uuid : itemUuid ,  restaurantUuid : restaurant.uuid}
+  });
+  
+  return res.send( item );
+});
+
+
+router.post('/item', async (req, res, /* next */) => {
+
+  const { restaurant } = req;
+  const {itemUuid} = req.query;
+  const payload = req.body;
+  // if item exists
+  if ( itemUuid !== undefined)
+  {
+    
+    await db.Item.update( 
+      {
+        name: payload.name, 
+        price : payload.price,
+        desc: payload.desc , 
+        metadata : payload.metadata ,
+        img: payload.img,
+        updatedAt: moment().format('YYYY-MM-DD HH:mm:ss')
+
+      }, {
+        where: {uuid : itemUuid ,  restaurantUuid : restaurant.uuid}
+      });
+     
+    return res.send({ success: true });
+  }
+  
+  await db.Item.create({
+    uuid: uuid(),
+    name: payload.name, 
+    price : payload.price,
+    desc: payload.desc , 
+    metadata : payload.metadata ,
+    //  img: payload.img,
+    restaurantUuid : restaurant.uuid,
+    createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+    updatedAt: moment().format('YYYY-MM-DD HH:mm:ss')
+  });
+  return res.send({ success: true });
+  
+});
+
+
+router.delete('/item', async (req, res, /* next */) => {
+
+  const {itemUuid} = req.query;
+
+  await db.Item.destroy({
+    where: {
+      uuid: itemUuid }
+  });
+  return res.send({ success: true });
+});
+
 
 module.exports = router;
